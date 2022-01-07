9f74b7b3df1711f9b930177f62871ac898de5c5c
spatterss135 <spatterss135@gmail.com>
2022-01-07 14:40:24 -0600

    MOMOMOMO

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
+  let [counter, changeMyDiaper] = useState(1)
+  window.counter = counter
+  window.changeCounter = changeMyDiaper
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>The counter is at {counter}</div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Or not what do I care.
         </p>
         <a
           className="App-link"
@@ -18,6 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <span>The counter is at {counter}</span>
     </div>
   );
 }
